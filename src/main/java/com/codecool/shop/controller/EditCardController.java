75be58db2de358494ce5ae5ef3651ce988e7d60e
garyAdam <rapture047@gmail.com>
2019-04-11 21:41:45 +0200

    edit done, separate buttons for quantity save

---
+++ src/main/java/com/codecool/shop/controller/EditCardController.java
@@ -0,0 +1,40 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/cart/edit"})
+public class EditCardController extends HttpServlet {
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession httpSession = req.getSession();
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+
+        int id = Integer.parseInt(req.getParameter("cartButton"));
+        Product chosenProduct = productDataStore.find(id);
+
+        Cart cart = (Cart) httpSession.getAttribute("cart");
+
+        for (int i=0; i< cart.productsInCart.size(); i++) {
+            if (cart.productsInCart.get(i).getProduct() == chosenProduct) {
+                cart.productsInCart.get(i).setQuantity(Integer.parseInt(req.getParameter("quantity")));
+            }
+        }
+
+
+        resp.sendRedirect("/shopping-cart");
+
+    }
+}
